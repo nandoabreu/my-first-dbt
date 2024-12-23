cf8459f0abd16bfea48d929fd7e6d2ac0febf135
Fernando Abreu <49952773+nandoabreu@users.noreply.github.com>
2024-12-23 14:57:29 +0000

    Update source reference

--- models/staging/net_data_model.sql
+++ models/staging/net_data_model.sql
@@ -6,5 +6,5 @@ SELECT
     net_data::jsonb->>'cxn' AS connection_type,
     (net_data::jsonb->>'in')::float AS inbound,
     (net_data::jsonb->>'out')::float AS outbound
-FROM sensor_data
+FROM {{ source('sensor_data_source', 'sensor_data') }}
 WHERE device <> '0' AND net_data IS NOT NULL
